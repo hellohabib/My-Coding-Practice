366b2f85cb2b0651e96a45dba3023119859ce495
hellohabib <habibdigitalmail@gmail.com>
2026-05-06 01:18:12 +0600

    3rd type function practiced No-Return + Parameter

--- module-14-extra-practice-prob3.c
+++ module-14-extra-practice-prob3.c
@@ -4,32 +4,29 @@
 // Take a number as input and check if it is an even number or odd. 
 // Do this 4 times with 4 types of functions:
 // 1. Return + Parameter 
-// 2. Return + No-Parameter - Done Here
-// 3. No-Return + Parameter
+// 2. Return + No-Parameter
+// 3. No-Return + Parameter - Done Here
 // 4. No-Return + No-Parameter
 #include<stdio.h>
-
-
-// 2. Return + No-Parameter
-
-char* isEvenOrOddType1String(){
-    int n;
-    scanf("%d", &n);
+// 3. No-Return + Parameter - Done Here
+char* isEvenOrOddType1String(int n){
+    
     if(n==0){
-        return "Unknown";
+        printf("%s", "Unknown");
     }
     else if (n%2==0){
-        return "Even";
+        printf("%s", "Even");
     }
     else {
-        return "Odd";
+        printf("%s", "Odd");
     }
 } 
 
 int main(){
-    
+    int n;
+    scanf("%d", &n);
     //printf("%s", isEvenOrOddType1Char(n)); 
-    printf("%s", isEvenOrOddType1String()); 
+    isEvenOrOddType1String(n);
 
     return 0;
 }
