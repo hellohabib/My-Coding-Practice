f782b0f28f1bcbaeb36505a3417556d47484030e
hellohabib <habibdigitalmail@gmail.com>
2026-05-14 01:31:17 +0600

    Recursion -এর মাধ্যমে n থেকে 1 পর্যন্ত প্রিন্ট করার জন্য C

---
+++ module-17-4-Print-from-1-to-N-using-recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void printNum(int i, int n){
+    if(i>n)
+    {
+        return;        
+    }
+    printNum(i+1,n);
+    printf("%d\n", i);       
+}
+
+int main(){
+    int n;
+    int i=1;
+    scanf("%dn", &n);
+    printNum(i,n);
+    return 0;
+}
